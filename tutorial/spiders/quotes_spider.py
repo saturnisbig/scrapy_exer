03456d69c7b6e88e32cfb2ac1e85d15faa76d5d0
Teddy <i.kenting@gmail.com>
2017-06-07 07:20:35 +0800

    使用response.follow跟踪链接，response.xpath解析内容

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,7 +2,6 @@
 # _*_ coding: utf-8 _*_
 
 import scrapy
-import lxml.html
 
 
 class QuoteSpider(scrapy.Spider):
@@ -12,8 +11,8 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        tree = lxml.html.fromstring(response.text)
-        tags = tree.xpath('//div[@class="quote"]')
+        # tree = lxml.html.fromstring(response.text)
+        tags = response.xpath('//div[@class="quote"]')
         for t in tags:
             text = t.xpath('./span[@class="text"]/text()')[0].strip(u'“”')
             author = t.xpath('./span/small/text()')[0]
@@ -21,10 +20,12 @@ class QuoteSpider(scrapy.Spider):
             quote = dict(author=author, text=text, tags=tags)
             yield quote
         try:
-            next_page = tree.xpath('//li[@class="next"]/a/@href')[0]
+            # next_page = tree.xpath('//li[@class="next"]/a')[0]
+            next_page = response.xpath('//li[@class="next"]/a/@href')[0]
         except IndexError:
             print(u'没有更多内容可以爬取了，当前URL：%s' % response.url)
         else:
-            url = response.urljoin(next_page)
-            yield scrapy.Request(url, callback=self.parse)
+            # url = response.urljoin(next_page)
+            # yield scrapy.Request(url, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
 
